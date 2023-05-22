ba897d6f4aec83c641a07d4175332768dd341efc
Linding3r <thomasjuullindinger@yahoo.dk>
2023-05-22 21:00:53 +0200

    Changed API url

--- settings.js
+++ settings.js
@@ -1,5 +1,4 @@
-export const API_URL = "https://jftr-gooffice.azurewebsites.net"
-//export const API_URL = "http://localhost:8080"
+export const API_URL = "https://gooffice-api.azurewebsites.net/"
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"
 
